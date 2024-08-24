8a7d9ebec6edfda6e00904f7f0b14cc95c185cee
yuuri-07 <133025383+yuuri-07@users.noreply.github.com>
2024-08-24 16:13:29 +0900

    Scene1.cpp

--- 覚え歌つくるくん/Scene1.cpp
+++ 覚え歌つくるくん/Scene1.cpp
@@ -1,4 +1,4 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 
 Scene1::Scene1(const InitData& init)
 	: IScene{ init }
@@ -11,23 +11,29 @@ Scene1::~Scene1()
 
 void Scene1::update()
 {
-	if (Button(Rect{ 335, 950, 300, 80 }, font, U"既存曲の曲設定", true))
-	{
-		changeScene(U"Scene3");
-	}
-
-	if (Button(Rect{ 785, 950, 300, 80 }, font, U"動画再生", true))
+	if (Button(Rect{ 0, 800, 350, 150 }, font, U"動画再生", true))
 	{
 		changeScene(U"Scene4");
 	}
 
-	if (Button(Rect{ 1235, 950, 300, 80 }, font, U"語句入力", true))
+	if (Button(Rect{ 0, 400, 350, 150 }, font, U"語句入力", true))
 	{
 		changeScene(U"Scene2");
 	}
+
+	if (Button(Rect{ 0, 600, 350, 150 }, font, U"曲選択", true))
+	{
+		changeScene(U"Scene3");
+	}
+
+	Rect{ 0, 200, 350, 150 }.draw();
+
+	font(U"OP").draw(40, Vec2{ 150, 245 }, ColorF{ 0.3, 0.7, 1.0 });
 }
 
 void Scene1::draw() const
 {
-	texture.draw(560, 70);
+	font(U"オープニング")
+		.draw(70, Rect{ 20, 20, 480, 200 }, Palette::White);
+	texture.draw(760, 130);
 }
