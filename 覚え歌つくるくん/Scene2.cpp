db81011968698770e050f3416ca9c1f2fe2cc094
s2201111 <s2201111@sendai.kosen-ac.jp>
2024-10-06 00:00:48 +0900

    列タイトルの変更

--- 覚え歌つくるくん/Scene2.cpp
+++ 覚え歌つくるくん/Scene2.cpp
@@ -1,7 +1,7 @@
 ﻿#include "Scene2.hpp"
 
 const Array<double> Scene2::minColumnWidths = { 50, 200, 200, 200, 200 };
-const Array<String> Scene2::columnNames = { U"番号", U"ことば*", U"よみ*", U"つけたし1", U"つけたし2" };
+const Array<String> Scene2::columnNames = { U"番号", U"ことば", U"よみ*", U"つけたし1", U"つけたし2" };
 
 Scene2::Scene2(const InitData& init)
 	: IScene{ init }, previousSelectedIndex{ s3d::none },
