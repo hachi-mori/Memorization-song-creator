38d953bb30c5d9aafbf323cd8e1446a40d27bd2b
s2201111 <s2201111@sendai.kosen-ac.jp>
2024-10-06 00:00:30 +0900

    再生画面の歌詞表示

--- 覚え歌つくるくん/Scene4.cpp
+++ 覚え歌つくるくん/Scene4.cpp
@@ -1,18 +1,110 @@
 ﻿#include "stdafx.h"
 
+const Array<double> Scene4::minColumnWidths = { 50, 200, 200, 200, 200 };
+const Array<String> Scene4::columnNames = { U"番号", U"ことば", U"よみ*", U"つけたし1", U"つけたし2" };
+
 Scene4::Scene4(const InitData& init)
-	: IScene{ init }, previousSelectedIndex{ s3d::none }, text{ U"" },audio{U""},playing{false}
+	: IScene{ init }, previousSelectedIndex{ s3d::none },
+	table{ minColumnWidths, {
+		.cellHeight = 45,
+		.variableWidth = true,
+	} }, text{ U"" }, audio{ U"" }, playing{ false }
 {
-	// Load Voice file names
+	InitializeTable();
+	UpdateListBoxState();
+	// 初期化
+	listBoxNeedsUpdate = true; // リストボックスの更新が必要なフラグを初期化
+}
+
+Scene4::~Scene4()
+{
+}
+
+void Scene4::InitializeTable()
+{
+	table.clear();
+	table.push_back_row(columnNames, Array<int32>(CellCountX + 1, 0));
+	table.setRowBackgroundColor(0, ColorF{ 0.9 });
+	tableScrollBar.moveTo(0);  // スクロール位置を一番上にリセット
+	for (int32 i = 1; i < (InitialCellCountY + 1); ++i)
+	{
+		AddNewRow();
+	}
+}
+
+void Scene4::AddNewRow()
+{
+	size_t rowIdx = table.rows();
+	Array<String> row(CellCountX + 1);
+	row[0] = U"{}"_fmt(rowIdx);
+
+	Array<int32> rowAlignments(CellCountX + 1, 1);
+	rowAlignments[0] = 0;
+
+	table.push_back_row(row, rowAlignments);
+	table.setBackgroundColor(rowIdx, 0, ColorF{ 0.9 });
+}
+
+void Scene4::UpdateListBoxState()
+{
+	Array<String> lyricsfileNames;
 	for (const auto& path : FileSystem::DirectoryContents(U"Voice"))
 	{
-		VoicefileNames << FileSystem::BaseName(path);
+		lyricsfileNames << FileSystem::BaseName(path);
 	}
-	listBoxState = ListBoxState(VoicefileNames);
+
+	listBoxState = ListBoxState(lyricsfileNames);
+}
+
+void Scene4::LoadCSVToTable(const String& filePath)
+{
+	// テーブルを初期化してから、CSVファイルの内容を読み込む
+	InitializeTable();  // 既存のテーブルをクリア
+
+	TextReader reader(filePath);
+	String line;
+	int32 rowIdx = 0;
+	// 行ごとに読み込む
+	while (reader.readLine(line))
+	{
+		Array<String> cells = line.split(U',');
+		for (int32 colIdx = 0; colIdx < cells.size(); ++colIdx)
+		{
+			if (rowIdx + 1 > 15 && colIdx + 1 == 1)
+			{
+				AddNewRow();
+			}
+			table.setText(Point{ colIdx + 1, rowIdx + 1 }, cells[colIdx]);
+		}
+		++rowIdx;
+	}
 }
 
 void Scene4::update()
 {
+	if (listBoxState.selectedItemIndex && listBoxState.selectedItemIndex != previousSelectedIndex)
+	{
+		// 選択されたアイテムの名前を取得
+		selectedVoiceFile = listBoxState.items[listBoxState.selectedItemIndex.value()];
+
+		// ハイフンで文字列を分割する
+		Array<String> parts = selectedVoiceFile.split(U'-');
+
+		// 歌詞を取得
+		String firstElement = parts[0];
+		TextReader reader(U"lyrics/" + firstElement + U".csv");
+
+		// te の内容を選択された要素の名前に変更
+		te.text = firstElement;
+		previousSelectedIndex = listBoxState.selectedItemIndex;
+		// スプレッドシートに内容を表示
+		LoadCSVToTable(U"lyrics/" + firstElement + U".csv");
+
+		audio = Audio{ Audio::Stream,  U"Voice/" + listBoxState.items[listBoxState.selectedItemIndex.value()] + U".wav",Loop::Yes };
+		audio.stop();
+		playing = { false };
+	}
+
 	// ボタンの処理
 	if (Scene1Button.mouseOver() && MouseL.down())
 	{
@@ -27,32 +119,10 @@ void Scene4::update()
 		changeScene(U"Scene3");
 	}
 
-	// リストボックスの選択が変更された場合にtrueを返す
-	if (listBoxState.selectedItemIndex != previousSelectedIndex)
-	{
-		previousSelectedIndex = listBoxState.selectedItemIndex;
-		// 選択が変更された場合の処理
-		if (listBoxState.selectedItemIndex.has_value())
-		{
-			// 選択されたアイテムの名前を取得
-			selectedVoiceFile = listBoxState.items[listBoxState.selectedItemIndex.value()];
-			
-			// ハイフンで文字列を分割する
-			Array<String> parts = selectedVoiceFile.split(U'-');
-
-			// 歌詞を取得
-			String firstElement = parts[0];
-			TextReader reader(U"lyrics/" + firstElement + U".csv");
-			text = reader.readAll();
-
-			audio = Audio{ Audio::Stream,  U"Voice/" + listBoxState.items[listBoxState.selectedItemIndex.value()] + U".wav",Loop::Yes };
-		}
-	}
-
 	// ファイルの再生
 	if (PlayButton.mouseOver() && MouseL.down())
 	{
-		if(listBoxState.selectedItemIndex){
+		if (listBoxState.selectedItemIndex) {
 			// オーディオを新しいファイルで初期化
 			if (!playing) {
 				audio.play();
@@ -65,7 +135,7 @@ void Scene4::update()
 		}
 		else {
 			System::MessageBoxOK(U"", U"おぼえうたをえらんでください");
-		}		
+		}
 	}
 
 	if (RePlayButton.mouseOver() && MouseL.down())
@@ -73,6 +143,7 @@ void Scene4::update()
 		if (listBoxState.selectedItemIndex) {
 			audio.stop();
 			audio.play();
+			playing = { true };
 		}
 		else {
 			System::MessageBoxOK(U"", U"おぼえうたをえらんでください");
@@ -83,17 +154,46 @@ void Scene4::update()
 	if (DeleteButton.mouseOver() && MouseL.down() && listBoxState.selectedItemIndex)
 	{
 		const MessageBoxResult result = System::MessageBoxOKCancel(U"", U"「" + listBoxState.items[listBoxState.selectedItemIndex.value()] + U"」\nこのファイルをけしますか？");
+		audio.stop();
+		playing = { false };
 
 		// OK が選ばれたら
 		if (result == MessageBoxResult::OK)
 		{
 			FileSystem::Remove(U"Voice/" + listBoxState.items[listBoxState.selectedItemIndex.value()] + U".wav");
+			UpdateListBoxState();
+			InitializeTable();
+			te.text = U"";
 			previousSelectedIndex = { s3d::none };
 		}
 	}
 
+	if (listBoxState.selectedItemIndex) {
+		//スプレッドシートの座標
+		const Rect tableViewport = Scene::Rect().stretched(-150).movedBy(810, 0);
+
+		tableScrollBar.updateLayout(tableViewport);
+		tableScrollBar.updateConstraints(0, table.height(), tableViewport.h);
+		if (tableViewport.mouseOver())
+		{
+			tableScrollBar.scroll(Mouse::Wheel() * 100);
+		}
+		tableScrollBar.update(tableViewport.mouseOver() ? Cursor::PosF() : MakeOptional<Vec2>());
+
+		{
+			ScopedViewport2D vp{ tableViewport };
+
+			Transformer2D tf{
+				Mat3x2::Translate(0, -tableScrollBar.value()),
+				Mat3x2::Translate(tableViewport.pos.movedBy(0, -tableScrollBar.value()))
+			};
+
+			table.draw();
+		}
+	}
+
 	//カーソル
-	if (Scene1Button.mouseOver()|| Scene2Button.mouseOver()|| Scene3Button.mouseOver()|| DeleteButton.mouseOver()|| PlayButton.mouseOver()|| RePlayButton.mouseOver())
+	if (Scene1Button.mouseOver() || Scene2Button.mouseOver() || Scene3Button.mouseOver() || DeleteButton.mouseOver() || PlayButton.mouseOver() || RePlayButton.mouseOver())
 	{
 		Cursor::RequestStyle(CursorStyle::Hand);
 	}
@@ -132,7 +232,8 @@ void Scene4::draw() const
 		//PlayButton
 		PlayButton.draw(buttonColor);
 		FontAsset(U"MainFont")(U"ながす").drawAt(Vec2{ PlayButton.center() }, Palette::White);
-	}else{
+	}
+	else {
 		System::Sleep(10);
 		StopButton.draw(buttonColor);
 		FontAsset(U"MainFont")(U"とめる").drawAt(Vec2{ StopButton.center() }, Palette::White);
@@ -140,5 +241,10 @@ void Scene4::draw() const
 
 	//RePlayButton
 	RePlayButton.draw(buttonColor);
-	FontAsset(U"MainFont")(U"さいしょからながす").drawAt(Vec2{ RePlayButton.center()}, Palette::White);
+	FontAsset(U"MainFont")(U"さいしょからながす").drawAt(Vec2{ RePlayButton.center() }, Palette::White);
+
+
+	tableScrollBar.draw();
+
+	if (listBoxState.selectedItemIndex)FontAsset(U"MainFont")(te.text +U"の覚え歌").drawAt(44, Vec2{1389, 116.7}, Palette::Black);
 }
--- 覚え歌つくるくん/Scene4.hpp
+++ 覚え歌つくるくん/Scene4.hpp
@@ -6,27 +6,68 @@ using App = SceneManager<String>;
 class Scene4 : public App::Scene
 {
 public:
+	// コンストラクタとデストラクタ
 	Scene4(const InitData& init);
+	~Scene4();
 
+	// 更新処理
 	void update() override;
+
+	// 描画処理
 	void draw() const override;
-	
 
 private:
+	// テーブルの初期設定
+	void InitializeTable();
 
+	// テーブルの行追加
+	void AddNewRow();
+
+	// リストボックスの更新
+	void UpdateListBoxState();
+
+	// CSVファイルの内容をスプレッドシートに表示
+	void LoadCSVToTable(const String& filePath);
+
+	// テーブルオブジェクト
+	SimpleTable table;
+
+	// スクロールバー
+	SasaGUI::ScrollBar tableScrollBar{ SasaGUI::Orientation::Vertical };
+
+	// リストボックスの状態
 	mutable ListBoxState listBoxState;
+	bool listBoxNeedsUpdate = true;
+
+	// 編集状態
+	Optional<Point> activeIndex;
+	Optional<Point> nextActiveIndex;
+	TextEditState textEditState;
+
+	// セルの最小幅と見出し
+	static const int32 CellCountX = 4;
+	static const int32 InitialCellCountY = 15;
+	static const Array<double> minColumnWidths;
+	static const Array<String> columnNames;
+
+	// テキストボックス
+	mutable TextEditState te;
+	s3d::Optional<uint64> previousSelectedIndex;
+
+	// 保存メッセージ
+	double MessageOpacity;
+
+	const ColorF buttonColor{ 0.3, 0.7, 1.0 };  // ボタンの共通色
+
 	Array<String> VoicefileNames;
 	String selectedVoiceFile;
 	Audio audio1{};
-	s3d::Optional<uint64> previousSelectedIndex;
 
 	const Rect Scene1Button{ 0, 900, 200, 100 };
 	const Rect Scene2Button{ 0, 230, 350, 150 };
 	const Rect Scene3Button{ 0, 430, 350, 150 };
 
 	mutable TextAreaEditState textAreaEditState;
-
-	const ColorF buttonColor{ 0.3, 0.7, 1.0 };  // ボタンの共通色
 
 	mutable String text;
 
